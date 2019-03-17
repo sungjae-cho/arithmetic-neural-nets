69aeea592d2680a640c0e9101323a4f030a57e67
Sungjae Cho <sungjae.cho.1118@gmail.com>
2019-03-17 20:58:46 +0900

    Edit test.sh for easy testing.

--- test.sh
+++ test.sh
@@ -1,8 +1,11 @@
-arg='test.py'
+experiment_name='test'
+operand_digits=4
+operator='add'
+rnn_type='jordan'
+activation='relu'
+hidden_units=64
+confidence_prob=0.9
+max_steps=30
+device_num=1
 
-func()
-{
-	python3 $arg
-}
-
-func
+python3 rnn_run.py $experiment_name $operand_digits $operator $rnn_type $activation $hidden_units $confidence_prob $max_steps $device_num
